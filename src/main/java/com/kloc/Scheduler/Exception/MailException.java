ee4c64355134808526e5f2af18eb345cbd8cc887
Ankush <ankush@kloctechnologies.com>
2023-07-03 18:18:31 +0530

    AnkushCode first Commit

--- src/main/java/com/kloc/Scheduler/Exception/MailException.java
+++ src/main/java/com/kloc/Scheduler/Exception/MailException.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value=HttpStatus.NOT_FOUND)
 public class MailException extends RuntimeException
 {
+	// this is for Exception Handling
 	private String resourceName;
 	private String filename;
 	private Object fieldvalue;
